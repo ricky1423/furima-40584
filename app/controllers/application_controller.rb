103baded605463db4409955d25bba010cf9dbe96
ricky1423 <rikisoccer0904@gmail.com>
2024-05-14 12:49:27 +0900

    エラーメッセージの表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,12 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
+      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD'] # 環境変数を読み込む記述に変更
     end
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :nickname, :name_first, :name_second, :name_first_kana, :name_second_kana, :birth_day])
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:email, :password, :nickname, :name_first, :name_second, :name_first_kana, :name_second_kana, :birth_day])
   end
 end
