b7432a9f33964ac8a642bf2f489926b874f111b5
ricky1423 <rikisoccer0904@gmail.com>
2024-05-15 16:09:46 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,9 +8,17 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :schedule
 
-  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :shopping_free_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :schedule_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :explain, presence: true
+
+  validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :condition_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shopping_free_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :schedule_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+
+  validates :price, presence: true,
+                    numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+                    format: { with: /\A\d+\z/, message: 'is not a number' }
 end
