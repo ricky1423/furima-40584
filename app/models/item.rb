7efc340f7c8d064161de372f09d6c44df6bf145e
ricky1423 <rikisoccer0904@gmail.com>
2024-05-15 15:54:06 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,16 @@
 class Item < ApplicationRecord
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_free
+  belongs_to :prefecture
+  belongs_to :schedule
+
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shopping_free_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :schedule_id, numericality: { other_than: 1, message: "can't be blank" }
 end
