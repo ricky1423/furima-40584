0dfdbc779a8309a311cad7c1bcd851b7506c1ac3
ricky1423 <rikisoccer0904@gmail.com>
2024-05-17 15:36:27 +0900

    商品一覧機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,8 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
 
   def index
+    @items = Item.all
+    @items = Item.order(created_at: :desc)
   end
 
   def new
