9f905ce1b17ef628f69fbff28f5ee09da5431c86
ricky1423 <rikisoccer0904@gmail.com>
2024-05-19 18:56:27 +0900

    Reapply "商品編集機能実装"
    This reverts commit 4f0c43587af48ceb675e927512b7e22da768e2c4.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
 
-  before_action :authenticate_user!, except: [:show, :index]
+  before_action :authenticate_user!, except: [:show, :index,]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -23,9 +24,30 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(items_params)
+    if @item.save
+      redirect_to item_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def items_params
     params.require(:item).permit(:image, :name, :explain, :category_id, :condition_id, :shopping_free_id, :prefecture_id, :schedule_id, :price).merge(user_id: current_user.id)
   end
+
+  def move_to_index
+    unless current_user.id == Item.find(params[:id]).user_id
+      redirect_to action: :index
+    end
+  end
 end
+
